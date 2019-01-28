71a5bca60c3aedb87db1a1e0c8ad8be42c342e0a
Dustin Jordan <redmaldeveloper@gmail.com>
2019-01-27 19:56:07 -0900

    consolidated code, rewrote loop comparison

--- YahooFrontpageNews.java
+++ YahooFrontpageNews.java
@@ -1,5 +1,5 @@
 /**
- * Created by Redmal on 1/21/2019.
+ * Created by Redmal on 1/27/2019.
  * grabs the headlines off Yahoo front page
  * that contain the word/topic a user enters
  */
@@ -16,35 +16,23 @@ public class YahooFrontpageNews {
 
     public static void main(String[] args) throws IOException {
         String url = "https://www.yahoo.com/";
-        int spanCount = 0;
-        int titleCount = 0;
-
-        Document document = Jsoup.connect(url).get();
-
-        Elements headers = document.select("*");
-
-        //get number of headers
-        for (Element ele : headers){
-            spanCount += 1;
-        }
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a topic: ");
+        System.out.print("Enter a news topic keyword: ");
         String getUserTopic = scanner.nextLine();
 
         System.out.println("-----Yahoo Front Page Headlines for " + getUserTopic + "-----\n\n");
 
-        //filter headers that contain getUserTopic
-        int numOfTitle = 0;
-        GrabHeadersLoop: for (Element span : headers){
-            if (span.text().toLowerCase().contains(getUserTopic.toLowerCase())) {
-                System.out.println((numOfTitle + 1) + ": " + span.text());
-                numOfTitle += 1;
-            }
+        Document document = Jsoup.connect(url).get();
+        Elements headers = document.getElementsByTag("span");
 
-            titleCount += 1;
-            if (titleCount == spanCount){
-                break GrabHeadersLoop;
+        int numOfTitle = 0;
+
+        //get number of headers
+        for (Element spanVar : headers) {
+            if (spanVar.text().toLowerCase().contains(getUserTopic.toLowerCase())) {
+                System.out.println((numOfTitle + 1) + ": " + spanVar.text());
+                numOfTitle += 1;
             }
         }
     }
